b64a251a57f5a9e334c697e7a888be19d54471b9
Wix Remote Machine <support@wix.com>
2026-02-09 22:46:31 +0000

    Fix page loading issue

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -1,5 +1,4 @@
 import { motion } from 'framer-motion';
-import { Image } from '@/components/ui/image';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import HeroSection from '@/components/sections/HeroSection';
