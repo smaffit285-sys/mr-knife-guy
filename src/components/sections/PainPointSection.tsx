6089e23950846ac66d97e762564706057053cf32
Wix Remote Machine <support@wix.com>
2026-02-27 17:23:56 +0000

    Double image height and width

--- src/components/sections/PainPointSection.tsx
+++ src/components/sections/PainPointSection.tsx
@@ -86,7 +86,7 @@ export default function PainPointSection() {
                 <div className="flex gap-4 sm:gap-8 justify-center items-end">
                   {/* Dull */}
                   <div className="flex flex-col items-center flex-1">
-                    <div className="w-full h-64 sm:h-80 mb-2 sm:mb-4 rounded-lg overflow-hidden">
+                    <div className="w-full h-128 sm:h-160 mb-2 sm:mb-4 rounded-lg overflow-hidden">
                       <Image
                         src="https://static.wixstatic.com/media/37d64c_fbeb3c16558c461b9d5d2420732b6ffc~mv2.jpg"
                         alt="Dull knife"
