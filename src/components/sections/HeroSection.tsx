d1c353e352e405e090ea5d7d9c908aff029b012f
Wix Remote Machine <support@wix.com>
2026-03-02 09:26:07 +0000

    Link SVG to Instagram

--- src/components/sections/HeroSection.tsx
+++ src/components/sections/HeroSection.tsx
@@ -10,7 +10,7 @@ const socialLogos = [
         <path d="M7 2h10a5 5 0 0 1 5 5v10a5 5 0 0 1-5 5H7a5 5 0 0 1-5-5V7a5 5 0 0 1 5-5zm5 4a4 4 0 1 0 0 8 4 4 0 0 0 0-8zm6-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"/>
       </svg>
     ),
-    url: 'https://instagram.com/miamiknifeguy',
+    url: 'http://www.instagram.com/MiamiKnifeGuy',
     color: '#E4405F',
   },
   {
